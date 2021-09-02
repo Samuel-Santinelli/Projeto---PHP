3f6cd4980c2348d0443624a9182f1a4394ee54cc
Samuel.S <samuelsantinelli@gmail.com>
2021-09-02 10:31:08 -0300

    Atualizando os arquivos

--- ParImpar/ParImpar.php
+++ ParImpar/ParImpar.php
@@ -1,10 +1,11 @@
 <?php
-
+    require_once('funcao.php');
 
 
 $cont = (int) 0;
 $exibirImpar = (int) 0;
 $exibirPar = (int) 0;
+
 
 
 $numeroInicial = (int) 0;
@@ -16,7 +17,7 @@ $numeroFinal = (int) 0;
 if(isset ($_POST['btnCalc'])){
     
     if( $_POST["stlPar"] = "" || $_POST["stlIm"] = "" ){
-        echo("caixa_vazia");
+        echo("Caixa vazia, por favor, selecione um número!!</h1>");
     }
     else{
         $numeroInicial = (int)$_POST['sltPar'];
@@ -25,91 +26,83 @@ if(isset ($_POST['btnCalc'])){
     
     if($numeroInicial >= $numeroFinal){ 
                         
-        echo("numero_menor");
+        echo("<h1>O numero selecionado é menor!!</h1>");
     
     }
 }
 
 
 
-
-
-
- 
-
-
 ?>
 
 
 <html>
-    
-    
-    
-    
-    <head>
-        <meta charset="utf-8">
-       <link rel="stylesheet"
-        type="text/css"
-        href="css/style.css">
-    </head>
-    
-    
-    
-    <body> 
-        
-        <h1> ImparPar </h1>
-        
-        <form name="frmParimp" method="post" action="">
-        <label> N-Inicial</label>
-            
+
+
+
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,100;1,300&display=swap" rel="stylesheet">
+</head>
+
+
+
+<body>
+
+    <header>ImparPar </header>
+
+    <form name="frmParimp" method="post" action="">
+        <label> <h1>N-Inicial</h1></label>
+
         <select name="sltPar">
-            
-  <?php  
+
+            <?php  
         for($cont=0;$cont<=500;$cont++){
             
         echo "<option value='$cont'>$cont  </option>"; 
     }
   
   ?>
-            
-  </select>
-            
-            
-        
-      <label> N-Final</label>
-            
+
+        </select>
+
+
+
+        <label> <h2>N-Final</h2></label>
+
         <select name="sltIm">
-            
-  <?php  
+
+            <?php  
         for($cont=100;$cont<=1000;$cont++){
             
         echo "<option value='$cont'>$cont  </option>"; 
     }
   
   ?>
-            
-  </select> 
+
+        </select>
         <input type="submit" name="btnCalc" value="Calcular">
-   
-            
-            
-    
-        
-    <table id="resultado">
-        
-        <tr>
-            <td id="tabela">
-                <?php
+
+
+        <table id="resultado">
+
+            <tr>
+                <td id="tabela">
+                    <?php
                     for($cont=$numeroInicial;$cont<=$numeroFinal;$cont++){
                         if ( ($cont % 2) == 0){
                             echo ($cont."<br>");
                     }
                 } 
                 ?>
-            </td>
-            
-            <td id="tabela1">
-                 <?php
+                </td>
+
+                <td id="segundaTabela">
+                    <?php
                 
                    for($cont=$numeroInicial;$cont<=$numeroFinal;$cont++){ 
                     
@@ -122,16 +115,16 @@ if(isset ($_POST['btnCalc'])){
                    }
                      
                   ?>
-            </td>
-        
-        
-        </tr>
-        
-        <tr>
-            
-            <td  class="quantidade">
-                
-                <?php
+                </td>
+
+
+            </tr>
+
+            <tr>
+
+                <td class="quantidade">
+
+                    <?php
                 
                 for($cont=$numeroInicial;$cont<=$numeroFinal;$cont++){
                     if(($cont %2 ) != 0 ){
@@ -141,20 +134,14 @@ if(isset ($_POST['btnCalc'])){
                 }
                 
                  echo ($exibirPar);
-                
-                
-                
-                
-            
-            ?>
-            
-            
-            </td>
-            
-            
-            <td class="quantidade" >
-                
-                <?php
+                    ?>
+
+                </td>
+
+
+                <td class="quantidade">
+
+                    <?php
                 
                 for($cont=$numeroInicial;$cont<=$numeroFinal;$cont++){
                     if(($cont %2 ) != 0  ){
@@ -166,26 +153,12 @@ if(isset ($_POST['btnCalc'])){
                   
             
             ?>
-            
-            
-            </td>
-            
-            
-            
-        
-        </tr>
-        
-        
-        
-    </table>
-    
-                
-        
-      
-    
-      </form>
-    </body>
 
 
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
 
 </html>
